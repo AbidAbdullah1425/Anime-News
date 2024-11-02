2470f9da127613c4c995010e79cc1eb5663d7661
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-11-02 19:30:07 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ class Bot(Client):
                 "root": "plugins"
             },
             workers=TG_BOT_WORKERS,
-            bot_token=TG_BOT_TOKEN
+            bot_token=TELEGRAM_TOKEN
         )
         self.LOGGER = LOGGER
 
