15ff0bf71f56ed5ae19908121c392f621e40b749
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-11-02 19:51:48 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
+import pyrogram.utils
+pyrogram.utils.MIN_CHANNEL_ID = -1002176591513
 
 from config import API_HASH, APP_ID, LOGGER, TELEGRAM_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 
